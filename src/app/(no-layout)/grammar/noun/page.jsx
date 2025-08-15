44d1f957c7f725135eb128e2bbeec6b0cd70bd03
Mohamad_Gomar <Mohamad_Gomar@gmail.com>
2025-08-16 00:10:58 +0330

    Lenis Deleted

--- src/app/(no-layout)/grammar/noun/page.jsx
+++ src/app/(no-layout)/grammar/noun/page.jsx
@@ -14,27 +14,15 @@ function Noun() {
 
   return (
 
-   <ReactLenis
-      options={{
-         duration: 4, // Animation duration in seconds
-         easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // Custom easing function
-         lerp: 0, // Linear interpolation intensity (0 to 1)
-         smoothWheel: true, // Enable smooth scrolling for mouse wheel
-         smoothTouch: true, // Disable smooth scrolling for touch (optional, can be unstable on iOS < 16)
-         wheelMultiplier: 1, // Adjust scroll speed for mouse wheel
-         touchMultiplier: 0.3, // Adjust scroll speed for touch
-         infinite: false, // Enable infinite scrolling (requires syncTouch: true for touch devices)
-      }}
-      root
-   >
-      <div className={styles.container}>
 
+   <div className={styles.container}>
 
-         <Link href="/grammar" className={styles.backHolder}>
-            <IoIosArrowBack className={styles.backSign} />
-            <div className={styles.backText}>Back</div>
-         </Link>
+      <Link href="/grammar" className={styles.backHolder}>
+         <IoIosArrowBack className={styles.backSign} />
+         <div className={styles.backText}>Back</div>
+      </Link>
 
+      <div className={styles.learningTrack}>
          <div className={styles.leftScroll}></div>
          <div className={styles.rightScroll}></div>
          <div className={styles.leftScroll2}></div>
@@ -71,9 +59,10 @@ function Noun() {
                A noun is any word related to objects, places, names and concepts.
             </div>
          </div>
-      
       </div>
-   </ReactLenis>
+
+   
+   </div>
 
   );
 }
