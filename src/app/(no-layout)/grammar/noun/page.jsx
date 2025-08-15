af4d136d79c18a0e6a37742342bd2fede8996dc8
Mohamad_Gomar <Mohamad_Gomar@gmail.com>
2025-08-15 23:12:29 +0330

    Lenis Settings

--- src/app/(no-layout)/grammar/noun/page.jsx
+++ src/app/(no-layout)/grammar/noun/page.jsx
@@ -13,21 +13,22 @@ function Noun() {
 
 
   return (
+
    <ReactLenis
       options={{
-      duration: 2, // Animation duration in seconds
-      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // Custom easing function
-      lerp: 0, // Linear interpolation intensity (0 to 1)
-      smoothWheel: true, // Enable smooth scrolling for mouse wheel
-      smoothTouch: true, // Disable smooth scrolling for touch (optional, can be unstable on iOS < 16)
-      wheelMultiplier: 1, // Adjust scroll speed for mouse wheel
-      touchMultiplier: 1, // Adjust scroll speed for touch
-      infinite: false, // Enable infinite scrolling (requires syncTouch: true for touch devices)
-   }}
-   root
-  >
-
+         duration: 4, // Animation duration in seconds
+         easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // Custom easing function
+         lerp: 0, // Linear interpolation intensity (0 to 1)
+         smoothWheel: true, // Enable smooth scrolling for mouse wheel
+         smoothTouch: true, // Disable smooth scrolling for touch (optional, can be unstable on iOS < 16)
+         wheelMultiplier: 1, // Adjust scroll speed for mouse wheel
+         touchMultiplier: 0.3, // Adjust scroll speed for touch
+         infinite: false, // Enable infinite scrolling (requires syncTouch: true for touch devices)
+      }}
+      root
+   >
       <div className={styles.container}>
+
 
          <Link href="/grammar" className={styles.backHolder}>
             <IoIosArrowBack className={styles.backSign} />
@@ -72,8 +73,8 @@ function Noun() {
          </div>
       
       </div>
-
    </ReactLenis>
+
   );
 }
 
