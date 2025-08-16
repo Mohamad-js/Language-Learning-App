a4285e6c3cfa016b926fec3ec40237727e8b18e1
Mohamad_Gomar <Mohamad_Gomar@gmail.com>
2025-08-16 10:44:49 +0330

    Nouns Content Added

--- src/app/(no-layout)/grammar/noun/page.jsx
+++ src/app/(no-layout)/grammar/noun/page.jsx
@@ -4,7 +4,6 @@ import styles from './noun.module.css';
 import Link from 'next/link';
 import { IoIosArrowBack } from 'react-icons/io';
 import GradientText from '@/components/gradientText/GradientText';
-import { ReactLenis } from '@studio-freight/react-lenis';
 
 
 function Noun() {
@@ -28,6 +27,7 @@ function Noun() {
          <div className={styles.leftScroll2}></div>
          <div className={styles.rightScroll2}></div>
 
+
          <div className={styles.titleHolder}>
             <GradientText
                colors={["#40ffaa", "#9900ff", "#ff00c8", "#ff0040", "#ff00f2"]}
@@ -38,26 +38,23 @@ function Noun() {
                NOUNS
             </GradientText>
          </div>
-
-         <div className={styles.learningBlock}>
-            <div className={styles.sections}>
-               A noun is any word related to objects, places, names and concepts.
-            </div>
-            <div className={styles.sections}>
-               A noun is any word related to objects, places, names and concepts.
-            </div>
-            <div className={styles.sections}>
-               A noun is any word related to objects, places, names and concepts.
-            </div>
-            <div className={styles.sections}>
-               A noun is any word related to objects, places, names and concepts.
-            </div>
-            <div className={styles.sections}>
-               A noun is any word related to objects, places, names and concepts.
-            </div>
-            <div className={styles.sections}>
-               A noun is any word related to objects, places, names and concepts.
-            </div>
+         <div className={styles.sections}>
+            A noun is any word related to objects, places, names and concepts.
+         </div>
+         <div className={styles.sections}>
+            A noun is any word related to objects, places, names and concepts.
+         </div>
+         <div className={styles.sections}>
+            A noun is any word related to objects, places, names and concepts.
+         </div>
+         <div className={styles.sections}>
+            A noun is any word related to objects, places, names and concepts.
+         </div>
+         <div className={styles.sections}>
+            A noun is any word related to objects, places, names and concepts.
+         </div>
+         <div className={styles.sections}>
+            A noun is any word related to objects, places, names and concepts.
          </div>
       </div>
 
