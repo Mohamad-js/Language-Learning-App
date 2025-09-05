73385161b2e4ba377b201819fcf20c19f23486e8
Mohamad Gomar <143497193+Mohamad-js@users.noreply.github.com>
2025-09-06 01:00:30 +0330

    DC bug

--- src/app/(no-layout)/a1/page.jsx
+++ src/app/(no-layout)/a1/page.jsx
@@ -161,7 +161,7 @@ function A1() {
                <div className={styles.progressInfoHolder}>
                   <div className={styles.number}>{progress}% done</div>
                   <div className={styles.counter}
-                     style={{height: progress + '%'}}
+                     style={{width: progress + '%'}}
                   ></div>
 
                   <button className={styles.continue}>
