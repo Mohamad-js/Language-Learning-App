81f7221a48fc42221071e43f232532cb85bde9b8
Mohamad_Gomar <Mohamad_Gomar@gmail.com>
2026-09-20 12:33:27 +0330

    Quiz List Update

--- src/app/(extras)/quiz/page.jsx
+++ src/app/(extras)/quiz/page.jsx
@@ -3,6 +3,7 @@ import {useState, useEffect} from "react";
 import Image from 'next/image';
 import Back from '@/components/backButton/back'
 import { getAllQuizzes } from "@/lib/db";
+import {GoArrowRight} from "react-icons/go";
 
 
 export default function Quiz() {
@@ -50,13 +51,25 @@ export default function Quiz() {
             <div className='w-full min-h-0 overflow-auto p-5 flex-1 relative flex flex-col gap-3'>
                 <div className='w-full h-10 text-xl font-bold text-foreground'>Quiz Time</div>
 
-                <div className='w-full min-h-0 overflow-auto flex flex-col gap-3'>
+                <div className='w-full min-h-0 overflow-auto flex flex-col gap-5'>
                     {
                         quiz?.map((item, index) => (
-                            <div key={index} className='w-full p-3 bg-background/60 border rounded-2xl'>
-                                <div className='text-xs text-gray-500'>Quiz {item.quizNumber}</div>
-                                <div className='text-xl'>{item.quizTitle}</div>
-                                <div className='text-sm'>{item.featuring}</div>
+                            <div key={index} className='w-full p-5 bg-background/60 border rounded-2xl backdrop-blur-xs flex flex-col gap-5'>
+                                <div className='w-full flex justify-between'>
+                                    <div className='text-xs text-gray-500'>Quiz {item.quizNumber}</div>
+                                    
+                                    <div className='text-xs text-gray-500'>{item.quizLevel}</div>
+                                </div>
+                                
+                                <div className=''>
+                                    <div className='text-xl'>{item.quizTitle}</div>
+                                    <div className='text-sm'>{item.featuring}</div>
+                                </div>
+
+                                <div className="flex items-center justify-end gap-3">
+                                    <div className='text-sm font-semibold'>Start</div>
+                                    <GoArrowRight />
+                                </div>
                             </div>
                         ))
                     }
